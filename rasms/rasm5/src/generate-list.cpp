4fce8b025ceeb7d3475af2d4a6ddce14e48b76ac
Carlos Aguilera <18aguilerac@gmail.com>
2024-05-08 03:42:31 +0000

    update

--- rasms/rasm5/src/generate-list.cpp
+++ rasms/rasm5/src/generate-list.cpp
@@ -7,8 +7,16 @@ const int LIST_SIZE = 200000;
 int main() {
   std::ofstream outFile("input.txt");
 
+  /**
+   * A standard interface to a platform-specific non-deterministic
+   * random number generator (if any are available).
+   */
   std::random_device rseed;
+
+  // A generalized feedback shift register discrete random number generator.
   std::mt19937 rng(rseed());
+
+  // A discrete random distribution on the range @f$[min, max]@f$ with equal
   std::uniform_int_distribution<int> dist(INT32_MIN, INT32_MAX);
 
   for (int i = 0; i < LIST_SIZE; ++i) {
