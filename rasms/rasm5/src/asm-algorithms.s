e28804178aced5f669d8c5a2da1506dad36da2f7
Carlos Aguilera <18aguilerac@gmail.com>
2024-05-14 07:33:03 +0000

    update

--- rasms/rasm5/src/asm-algorithms.s
+++ rasms/rasm5/src/asm-algorithms.s
@@ -19,8 +19,38 @@ bubble_sort:
   MOV X19, X0  // move starting address of array
   MOV X20, X1  // move length of array into X20
 
-  LDR X0, =szAsmBubbleSort
-  BL putstring
+  MOV X0, #0            // set counter i
+  bubble_sort_loop:
+    CMP X0, X20          // is i < length
+    B.GE bubble_sort_loop_end
+
+    MOV X1, #0          // set up our j counter
+    SUB X2, X20, X0    
+    SUB X2, X2, #1
+    bubble_sort_inner_loop:
+      CMP X1, X2          // is j < length - i - 1
+      B.GE bubble_sort_inner_loop_end
+
+      MOV X3, X1      // set j + 1
+      ADD X3, X3, #1
+
+      LDR X4, [X19, X3]   // X4 = arr[j + 1]
+      LDR X5, [X19, X1]   // X5 = arr[j]
+
+      CMP X4, X5      // is X4 >= X5
+      B.GE bubble_sort_inner_loop_continue
+
+      STR X4, [X19, X1]   // a[j] = a[j + 1]
+      STR X5, [X19, X3]   // a[j + 1] = temp
+
+    bubble_sort_inner_loop_continue:
+      ADD X1, X1, #1   // ++j
+      B bubble_sort_inner_loop
+    bubble_sort_inner_loop_end:
+
+    ADD X0, X0, #1    // ++i
+    B bubble_sort_loop
+  bubble_sort_loop_end:
 
   LDR X20, [SP], #16 			// pop link register off the stack
   LDR X19, [SP], #16 			// pop link register off the stack
@@ -34,9 +64,6 @@ insertion_sort:
 
   MOV X19, X0  // move starting address of array
   MOV X20, X1  // move length of array into X20
-
-  LDR X0, =szAsmInsertionSort
-  BL putstring
 
   LDR X20, [SP], #16 			// pop link register off the stack
   LDR X19, [SP], #16 			// pop link register off the stack
